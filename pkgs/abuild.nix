f0f94c0eb1b6021be0f14560d2a592f1134640c4
maxice8 <thinkabit.ukim@gmail.com>
2022-03-07 22:58:00 -0300

    flake: override abuild

---
+++ pkgs/abuild.nix
@@ -0,0 +1,13 @@
+self: super:
+{
+  abuild = super.houdini.overrideAttrs ( old: rec {
+    version = "3.9.0";
+    src = super.fetchFromGitLab rec {
+      domain = "gitlab.alpinelinux.org";
+      owner = "alpine";
+      repo = "abuild";
+      rev = version;
+      sha256 = "1zs8slaqiv8q8bim8mwfy08ymar78rqpkgqksw8y1lsjrj49fqy4";
+    };
+  });
+}
