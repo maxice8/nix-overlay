aa1d077cd76e9009371525b927335d69f4ac9452
maxice8 <thinkabit.ukim@gmail.com>
2022-03-19 04:18:33 -0300

    ydotool: general fixes

--- pkgs/ydotool.nix
+++ pkgs/ydotool.nix
@@ -1,15 +1,14 @@
-{ pkgs, lib, stdenv, cmake, fetchFromGitHub }:
+{ stdenv
+, lib
+, fetchFromGitHub
+, cmake
+, scdoc
+, util-linux
+}:
 
 stdenv.mkDerivation rec {
   pname = "ydotool";
   version = "1.0.1";
-
-  meta = with lib; {
-    description = "client and daemon that uses uinput to simulate keypress";
-    homepage = "https://github.com/ReimuNotMoe/ydotool";
-    license = licenses.agpl3;
-    platforms = platforms.linux;
-  };
 
   src = fetchFromGitHub {
     owner = "ReimuNotMoe";
@@ -18,8 +17,21 @@ stdenv.mkDerivation rec {
     hash = "sha256-maXXGCqB8dkGO8956hsKSwM4HQdYn6z1jBFENQ9sKcA=";
   };
 
-  buildInputs = [
-    pkgs.cmake
-    pkgs.scdoc
+  nativeBuildInputs = [
+    cmake
+    scdoc
   ];
+
+  # fix call of /usr/bin/kill
+  postInstall = ''
+    substituteInPlace ${placeholder "out"}/lib/systemd/user/ydotool.service \
+      --replace '/usr/bin/kill' '${util-linux}/bin/kill'
+  '';
+
+  meta = with lib; {
+    description = "client and daemon that uses uinput to simulate keypress";
+    homepage = "https://github.com/ReimuNotMoe/ydotool";
+    license = licenses.agpl3;
+    platforms = platforms.linux;
+  };
 }
