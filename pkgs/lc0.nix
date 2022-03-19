1b5a67d7f0e74b56336d146fabc831c22eca1507
maxice8 <thinkabit.ukim@gmail.com>
2022-03-18 23:40:58 -0300

    lc0: apply patches for Policy level 3

--- pkgs/lc0.nix
+++ pkgs/lc0.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 , meson
 , ninja
 , pkg-config
@@ -49,6 +50,21 @@ stdenv.mkDerivation rec {
     "-Ddnnl_dir=${dnnl}"
     "-Donednn=false"
     "-Dgtest=false"
+  ];
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/Tilps/lc0/commit/6278b4501a3bfa125c7ebd564f2a51c749568d75.patch";
+      sha256 = "sha256-5XRBN/8a6aCVECBzIVDfuc5ooQRXSGl/+0z46HA/JN4=";
+    })
+    (fetchpatch {
+      url = "https://github.com/Tilps/lc0/commit/0a778d515b331de3da9714d2906439a2b4a58280.patch";
+      sha256 = "sha256-di5q8PQJ1Mu6JWnze/+8FYElWTAdA/KgrNDW833OZSc=";
+    })
+    (fetchpatch {
+      url = "https://github.com/Tilps/lc0/commit/9b12bf435e67cff71f42aa3e77c2b18e59840257.patch";
+      sha256 = "sha256-tkA2Equ6RnDQC5D26Ob2dHJpiBu+I/SEuWDOmyJR/k4=";
+    })
   ];
 
   # This is called during the build process
