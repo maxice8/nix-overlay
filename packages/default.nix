0944769d3139bd1780b8f600ba4a3c4339d40c89
maxice8 <thinkabit.ukim@gmail.com>
2022-03-25 03:38:41 -0300

    packages: provide a default.nix that can be imported

---
+++ packages/default.nix
@@ -0,0 +1,16 @@
+system: inputs:
+let
+  inherit inputs system;
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+    config = { allowUnfree = true; };
+  };
+in
+{
+  ydotool = pkgs.callPackage ./ydotool { src = inputs.ydotool-src; };
+  abuild = pkgs.callPackage ./abuild { src = inputs.abuild-src; };
+} // pkgs.lib.optionalAttrs (system == "x86_64-linux") {
+  # This package requires Intel's MKL which is only availble on
+  # x86_64-linux
+  lc0 = pkgs.callPackage ./lc0 { src = inputs.lc0-src; };
+}
