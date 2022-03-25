d2b8aa450a8e70fef86bc10ec2caab432fd18815
maxice8 <thinkabit.ukim@gmail.com>
2022-03-25 04:16:50 -0300

    flake: simplify even more

--- flake.nix
+++ flake.nix
@@ -50,79 +50,19 @@
 
   outputs =
     { self
-    , nixpkgs
-    , flake-utils
     , ...
     }@inputs:
     let
-      # Define system as defined by flake-utils
-      sys = flake-utils.lib.system;
-      # List all linux systems using the ${system} definition
-      # as that allows us to type-safely instead of relying on
-      # not typoing a string
-      linuxSystems =
-        [
-          sys.x86_64-linux
-          sys.i686-linux
-          sys.aarch64-linux
-        ];
+      inherit (inputs.flake-utils.lib) eachSystem system;
     in
     # Loop over all the linux systems
-    flake-utils.lib.eachSystem linuxSystems
-      (system:
+    eachSystem [ system.x86_64-linux system.i686-linux system.aarch64-linux ]
+      (currentSystem:
       {
-        packages = import ./packages system inputs;
-      }) //
-
-    # Define an overlay for each of the default systems
-    flake-utils.lib.eachDefaultSystem (system:
-    {
-      overlays =
-        final:
-        prev:
-        let
-          # Define a helper function calld 'buildMaxPlugin',
-          # that uses the 'buildVimPluginFrom2Nix' that is
-          # available on 'vimUtils'
-          #
-          # This function takes a 'name', that 'name' must
-          # be in the 'inputs' of the flake.nix
-          inherit (prev.vimUtils) buildVimPluginFrom2Nix;
-          buildMaxPlugin = name: buildVimPluginFrom2Nix {
-            pname = name;
-            version = "master";
-            src = builtins.getAttr name inputs;
-          };
-
-          # Create a list of plugins by reading all inputs
-          # and ignoring certain input names like 'self'
-          # and 'nixpkgs' since those are not meant to be
-          # vim plugins
-          #
-          # we also ignore ones that end with `-src` since
-          # those are sources that are passed to builds
-          plugins = builtins.filter
-            (name:
-              name != "self" &&
-              name != "nixpkgs" &&
-              name != "flake-utils" &&
-              !final.lib.hasSuffix "-src" name)
-            (builtins.attrNames inputs);
-        in
-        {
-          # Add the ydotool package if our system is inside
-          # the list of systems that are linux
-          ydotool = final.lib.mkIf (builtins.elem system linuxSystems)
-            self.packages.${system}.ydotool;
-          lc0 = final.lib.mkIf (builtins.elem system [ sys.x86_64-linux ])
-            self.packages.${system}.lc0;
-          abuild = final.lib.mkIf (builtins.elem system linuxSystems)
-            self.packages.${system}.abuil;
-
-          # UPDATE vimPlugins with the list of plugins
-          vimPlugins = prev.vimPlugins //
-          builtins.listToAttrs
-            (map (name: { inherit name; value = buildMaxPlugin name; }) plugins);
+        packages = import ./packages currentSystem inputs;
+        overlays = import ./overlays {
+          system = currentSystem;
+          packages = self.packages.${currentSystem};
         };
-    });
+      });
 }
