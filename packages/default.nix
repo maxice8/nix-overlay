2b13104885931215463d98721edecf261860f120
maxice8 <thinkabit.ukim@gmail.com>
2022-03-25 03:45:00 -0300

    packages: use flake-utils

--- packages/default.nix
+++ packages/default.nix
@@ -9,7 +9,7 @@ in
 {
   ydotool = pkgs.callPackage ./ydotool { src = inputs.ydotool-src; };
   abuild = pkgs.callPackage ./abuild { src = inputs.abuild-src; };
-} // pkgs.lib.optionalAttrs (system == "x86_64-linux") {
+} // pkgs.lib.optionalAttrs (system == inputs.flake-utils.lib.system.x86_64-linux) {
   # This package requires Intel's MKL which is only availble on
   # x86_64-linux
   lc0 = pkgs.callPackage ./lc0 { src = inputs.lc0-src; };
